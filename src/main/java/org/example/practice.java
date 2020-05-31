b9388c0b98c6f384c6819f3df88ad8409803e965
yasmin <yjones@spartaglobal.com>
2020-05-31 11:11:00 +0100

    first commit

---
+++ src/main/java/org/example/practice.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class practice {
+
+    int [] numbersArray = new int [20];
+
+    int [] myAge = new int [23];
+
+    int[] myArrayLiterally = new int [] {1,2,3,4,5,6,7,8,9,10};
+
+
+    public static void main(String[] args) {
+
+         int [] myArrayLiterally = new int[] {1,2,3,4,5,6};
+
+        for (int i = 0; i < myArrayLiterally.length; i++);
+
+        System.out.println(myArrayLiterally[3]);
+
+    }
+
+
+
+
+
+
+
+
+}
